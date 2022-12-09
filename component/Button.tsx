c28aab7f1d27784dd3f242ac3b99aaeed940750e
Shubham Surve <shubham@inradius.in>
2022-12-09 14:04:50 +0530

    updated

--- component/Button.tsx
+++ component/Button.tsx
@@ -12,12 +12,11 @@ import {
   FacebookLoginButton,
 } from "react-social-login-buttons";
 
-import * as queryString from 'query-string';
+import * as queryString from "query-string";
 
-
+const urlParams = queryString.parse(window.location.search);
 
 const REDIRECT_URI = "http://localhost:3000/account/login";
-
 
 const Button = () => {
   const [provider, setProvider] = useState("");
@@ -37,17 +36,56 @@ const Button = () => {
 
   const onLogout = useCallback(() => {}, []);
 
+  // const stringifiedParams = queryString.stringify({
+  //   client_id: "524305576404113",
+  //   redirect_uri: 'https://cheerful-dodol-9da0e8.netlify.app/fb',
+  //   scope: ['email', 'public_profile'].join(','), // comma seperated string
+  //   response_type: 'code',
+  //   auth_type: 'rerequest',
+  //   display: 'popup',
+  // });
 
-  const stringifiedParams = queryString.stringify({
+  const LinkendinParams = queryString.stringify({
+    client_id: "78uizzc35dmas4",
+    redirect_uri: "http://localhost:3000/loginn2",
+    scope: ["r_emailaddress", "r_liteprofile", "w_member_social"].join(","), // comma seperated string
+    response_type: "code",
+    auth_type: "rerequest",
+    display: "popup",
+  });
+
+  const FacebookParams = queryString.stringify({
     client_id: "524305576404113",
-    redirect_uri: 'http://localhost:3000/fb',
-    scope: ['email', 'public_profile'].join(','), // comma seperated string
-    response_type: 'code',
-    auth_type: 'rerequest',
-    display: 'popup',
+    redirect_uri: "http://localhost:3000/loginn2",
   });
   
-  const facebookLoginUrl = `https://www.facebook.com/v4.0/dialog/oauth?${stringifiedParams}`;
+  const FacebookUri = `https://www.facebook.com/v15.0/dialog/oauth?${FacebookParams}`;
+  const LinkendinUri = `https://www.linkedin.com/oauth/v2/authorization?${LinkendinParams}`;
+
+  console.log(`The code is: ${urlParams.code}`);
+
+  let codes = urlParams.code;
+
+  // async function getAccessTokenFromCode() {
+  //   const { data } = await axios({
+  //     url: `https://www.linkedin.com/oauth/v2/accessToken`,
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/x-www-form-urlencoded",
+  //     },
+  //     data: {
+  //       grant_type: "authorization_code",
+  //       client_id: "78uizzc35dmas4",
+  //       client_secret: "95j2Ok6MQD1HQEFL",
+  //       redirect_uri: "http://localhost:3000/loginn2",
+  //       code: urlParams.code,
+  //     },
+  //   });
+  //   console.log(data); // { access_token, token_type, expires_in }
+  //   return data.access_token;
+  // }
+
+  // const facebookLoginUrl = `https://www.facebook.com/v4.0/dialog/oauth?${stringifiedParams}`;
 
   return (
     <div className={`App ${provider && profile ? "hide" : ""}`}>
@@ -70,47 +108,44 @@ const Button = () => {
         <GoogleLoginButton />
       </LoginSocialGoogle>
 
-      {/* <LoginSocialLinkedin
-        client_id={"78d8hriyw21ckg"}
-        client_secret={"5IxoFanSYWtgycNl"}
-        redirect_uri={REDIRECT_URI2}
+      <LoginSocialLinkedin
+        client_id={"78uizzc35dmas4"}
+        client_secret={"95j2Ok6MQD1HQEFL"}
+        redirect_uri="http://localhost:3000/loginn2"
         onLoginStart={onLoginStart}
         onResolve={({ provider, data }: IResolveParams) => {
           setProvider(provider);
           setProfile(data);
-          console.log(data,provider)
+          console.log(data, provider);
         }}
         onReject={(err: any) => {
           console.log(err);
         }}
       >
         <LinkedInLoginButton />
-      </LoginSocialLinkedin> */}
+      </LoginSocialLinkedin>
 
       <LoginSocialFacebook
-          appId={"524305576404113"}
-          fieldsProfile={
-            'id,first_name,last_name,middle_name,name,name_format,picture,short_name,email,gender'
-          }
-          onLoginStart={onLoginStart}
-          
-          redirect_uri={REDIRECT_URI}
-          onResolve={({ provider, data }: IResolveParams) => {
-            setProvider(provider);
-            setProfile(data);
-          }}
-          onReject={err => {
-            console.log(err);
-          }}
-        >
-          <FacebookLoginButton />
-        </LoginSocialFacebook>
+        appId={"524305576404113"}
+        fieldsProfile={
+          "id,first_name,last_name,middle_name,name,name_format,picture,short_name,email,gender"
+        }
+        onLoginStart={onLoginStart}
+        redirect_uri={REDIRECT_URI}
+        onResolve={({ provider, data }: IResolveParams) => {
+          setProvider(provider);
+          setProfile(data);
+        }}
+        onReject={(err) => {
+          console.log(err);
+        }}
+      >
+        <FacebookLoginButton />
+      </LoginSocialFacebook>
 
-
-        <a href={facebookLoginUrl}>
-    Login with Facebook
-  </a>
-
+      <a href={LinkendinUri}>Login with Linkendin</a>
+      <br></br>
+      <a href={FacebookUri}>Login with Facebook</a>
     </div>
   );
 };
